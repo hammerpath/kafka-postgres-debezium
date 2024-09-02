147d2230253c1e7f8c20db56606af4e6f3d696ad
André Hammarstig <andre.hammarstig@ahlsell.se>
2024-09-02 20:46:42 +0200

    show topic content on a web page

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,4 +1,13 @@
+import express from "express";
+import http from "http";
 import { Kafka } from "kafkajs";
+import { WebSocketServer } from "ws";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const server = http.createServer(app);
 
 const kafkaBrokers = ["kafka:9092"];
 
@@ -11,12 +20,18 @@ const consumer = kafka.consumer({ groupId: "web-pinger-v1" });
 await consumer.connect();
 await consumer.subscribe({ topic: "web-ping", fromBeginning: true });
 
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log("message received", {
-      partition,
-      offset: message.offset,
-      value: message.value.toString(),
-    });
-  },
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", async (ws) => {
+  console.log("New client connected!");
+  // TODO - this can't be called by a new browser window since the consumer is already running.
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      ws.send(message.value.toString());
+    },
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
